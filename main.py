a40d3a5546b5143d2c59cca4e8a80117bb2ecfbe
edric <edriccoelho04@gmail.com>
2026-01-23 12:25:11 +0530

    Added embeddings and FAISS vector store

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from src.loader import load_documents
 from src.chunker import chunk_documents
+from src.vector_store import create_vector_store
 
 
 def main():
     docs = load_documents()
     chunks = chunk_documents(docs)
 
-    print(f"Loaded {len(docs)} documents")
-    print(f"Created {len(chunks)} chunks\n")
+    vector_store = create_vector_store(chunks)
 
-    for i, chunk in enumerate(chunks[:5]):  # show first 5 only
-        print(f"Chunk {i+1}:")
-        print(chunk.page_content)
-        print("-" * 40)
+    print(f"Loaded {len(docs)} docs")
+    print(f"Created {len(chunks)} chunks")
+    print("Vector store created successfully!")
 
 
 if __name__ == "__main__":
